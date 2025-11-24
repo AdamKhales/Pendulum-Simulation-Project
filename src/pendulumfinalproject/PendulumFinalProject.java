5fc97707e107c44447ce5b0d0cfa2cffd096d2fb
Adam Khales <2493077@edu.vaniercollege.qc.ca>
2025-11-24 09:59:30 -0500

    First commit

---
+++ src/pendulumfinalproject/PendulumFinalProject.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pendulumfinalproject;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author 2493077
+ */
+public class PendulumFinalProject extends Application{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        stage.show();
+    }
+    
+}
