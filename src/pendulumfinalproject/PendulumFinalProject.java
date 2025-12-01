10c78b3d15272c0729b7c99a0f6147c2bf84b419
AdamKhales <adamkhales2006@gmail.com>
2025-11-30 21:32:51 -0500

    Add repository link

--- src/pendulumfinalproject/PendulumFinalProject.java
+++ src/pendulumfinalproject/PendulumFinalProject.java
@@ -2,6 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
+
+//https://github.com/AdamKhales/Pendulum-Simulation-Project.git
+
 package pendulumfinalproject;
 
 import java.io.IOException;
